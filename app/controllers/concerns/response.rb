6fe9a76b304ca3826b0c1e954772287ed258c808
jsip <jsimonroyer@gmail.com>
2021-12-20 14:40:59 -0500

    array support for hashtags and mentions

--- app/controllers/concerns/response.rb
+++ app/controllers/concerns/response.rb
@@ -1,12 +1,20 @@
 module Response
   def json_response(object, status = :ok)
     csv_to_array(object)
+    puts object.inspect
     render json: object, status: status
   end
 
   private
 
   def csv_to_array(object)
-    # temp
+    if object.is_a?(Array)
+      object.each do |obj|
+        csv_to_array(obj)
+      end
+    elsif object.is_a?(TrumpTweet)
+      object.hashtags = object.hashtags.split(',')
+      object.mentions = object.mentions.split(',')
+    end
   end
 end
